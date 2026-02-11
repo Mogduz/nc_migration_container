1fd6bb4d4c62fb735cb7a419a30aa7e379479174
Mogduz <gerald.leikam@aol.com>
2026-02-11 11:15:13 +0100

    Create 20-apache.sh

---
+++ all_in_one/entrypoint.d/20-apache.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+log "Configuring Apache for Nextcloud"
+sed -i 's#DocumentRoot /var/www/html#DocumentRoot /var/www/html/nextcloud#' /etc/apache2/sites-available/000-default.conf
+cat > /etc/apache2/conf-available/nextcloud.conf <<'CONF'
+<Directory /var/www/html/nextcloud>
+  Require all granted
+  AllowOverride All
+  Options FollowSymLinks MultiViews
+</Directory>
+CONF
+cat > /etc/apache2/conf-available/nextcloud-redirect.conf <<'CONF'
+RedirectMatch 302 ^/nextcloud/?$ /
+CONF
+a2enconf nextcloud >/dev/null
+a2enconf nextcloud-redirect >/dev/null
