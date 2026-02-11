f0ab172f0b8ce0f4767788ca0b4080854707f821
Mogduz <gerald.leikam@aol.com>
2026-02-11 11:40:28 +0100

    Update 20-apache.sh

--- all_in_one/entrypoint.d/20-apache.sh
+++ all_in_one/entrypoint.d/20-apache.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+log "Running 20-apache.sh"
 log "Configuring Apache for Nextcloud"
 sed -i 's#DocumentRoot /var/www/html#DocumentRoot /var/www/html/nextcloud#' /etc/apache2/sites-available/000-default.conf
 cat > /etc/apache2/conf-available/nextcloud.conf <<'CONF'
