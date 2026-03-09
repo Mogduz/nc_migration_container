19cdbac5bb1a1527e4e5a99a93bfe006604c611b
Mogduz <gerald.leikam@aol.com>
2026-03-09 10:48:51 +0100

    deleted

--- single/entrypoint.d/00-env.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-# Zentrale Pfade fuer Nextcloud-Migrationslauf.
-NC_PATH="/var/www/html/nextcloud"
-NC_CONFIG_DIR="$NC_PATH/config"
-
-# Standardwerte fuer Migration.
-: "${NC_DATA_DIR:=/mnt/NextCloud/data}"
